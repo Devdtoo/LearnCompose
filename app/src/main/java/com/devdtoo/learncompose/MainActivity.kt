5de42a2e7044728690c2dc3ece105f982dd25e6a
Dev Chaturvedi <dev@Devs-MacBook-Air.local>
2022-04-18 19:28:36 +0530

    Text Styling

--- app/src/main/java/com/devdtoo/learncompose/MainActivity.kt
+++ app/src/main/java/com/devdtoo/learncompose/MainActivity.kt
@@ -15,9 +15,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.devdtoo.learncompose.ui.theme.LearnComposeTheme
@@ -43,10 +49,33 @@ class MainActivity : ComponentActivity() {
                     .background(Color(0xFF101010))
             ){
                 Text(
-                    text = "Jetpack Compose",
+                    text = buildAnnotatedString {
+                                                withStyle(
+                                                    style = SpanStyle(
+                                                        color = Color.Green,
+                                                        fontSize = 50.sp
+                                                    )
+                                                ){
+                                                    append("J")
+                                                }
+                        append("etpack ")
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 50.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose")
+                    },
                     color = Color.White,
                     fontSize = 30.sp,
-                    fontFamily = fontFamily
+                    fontFamily = fontFamily,
+                    fontWeight = FontWeight.Bold,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline,
                 )
             }
             LearnComposeTheme {
